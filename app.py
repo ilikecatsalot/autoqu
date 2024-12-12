9a14a2db8ea4bc2fad69e2889269a00f8b3ed89d
ilikecatsalot <111728441+ilikecatsalot@users.noreply.github.com>
2024-12-12 23:54:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS 
+from flask_cors import CORS
 import spacy
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
